47c5f34236ab5832f7e2bb27220e827fe8231acf
Por123430 <por123430@gmail.com>
2023-03-03 02:35:54 +0700

    แก้เปลี่ยน cors

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,7 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const baseQuery = fetchBaseQuery({
+    baseUrl : 'http://localhost:3500',
+    credentials: 'include',
+    prerareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token
+
+        if (token) {
+            headers.set("authorization", `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+const baseQueryWitchReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions)
+}
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
+    baseQuery,
     tagTypes: ['Note', 'User', 'Moniters'],
     endpoints: builder => ({})
 })
