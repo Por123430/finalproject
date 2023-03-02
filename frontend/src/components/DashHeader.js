27024e92b1c9f4029e7cde0aa9a3b90b3f5cb20e
Por123430 <por123430@gmail.com>
2023-03-02 14:46:49 +0700

    เพิ่ม login แต่ยังไม่ได้ทำ api

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,17 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../styles/DashHeader.css";
 import Logo from "../img/Egat-Logo.png";
+
+import { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, Link, useLocation } from "react-router-dom"
+
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
+
+
 const DashHeader = () => {
+
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const [sendLogout, {
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }] = useSendLogoutMutation()
+
+  useEffect(() => {
+    if (isSuccess) navigate('/')
+  }, [isSuccess, navigate])
+
+  const onLogoutClicked = () => sendLogout()
+
+  if (isLoading) return <p>Logging Out...</p>
+
+  if (isError) return <p>Error: {error.data?.message}</p>
+
+  let dashClass = null
+  if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+    dashClass = "dash-header__container--small"
+  }
+
+  const logoutButton = (
+    <button 
+      className="icon-button"
+      title="logout"
+      onClick={onLogoutClicked}
+    >
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  )
   const content = (
     <header className="dash-header">
-      <div className="dash-header__container">
+      <div className={`dash-header__container ${dashClass}`}>
         <nav className="dash-header__logo">
           <img src={Logo} alt="Logo"></img>
         </nav>
+        
         <Link to="/dash/notes" className="dash-header__title" >
           DataCenter
         </Link>
+        {logoutButton}
       </div>
     </header>
   );
---
+++ frontend/src/features/auth/authApiSlice.js
@@ -0,0 +1,39 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "auth/logout",
+        method: "POST",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(logOut());
+          console.log(data);
+          dispatch(apiSlice.util.resetApiState());
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+    refresh: builder.mutation({
+      query: () => ({
+        url: "/auth/refresh",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
+  authApiSlice;
