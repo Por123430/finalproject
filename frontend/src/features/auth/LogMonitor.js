d2e0ba4b8ed39ab57cf402a13d6530a79dab7f17
Por123430 <por123430@gmail.com>
2023-03-06 17:03:45 +0700

    efs

--- frontend/src/features/auth/LogMonitor.js
+++ frontend/src/features/auth/LogMonitor.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+
 const LogMonitor = () => {
   const content = (
     <section className="LogMonitor">
       <div className="LogMonitor-display">
         <div className="LogMonitor-display__item">
-            b1
+        <iframe src="http://127.0.0.1:1880/ui/#!/0?socketid=Df61a9GwoU9YfidMAAAJ" height="600" width="400"></iframe>
         </div>
         <div className="LogMonitor-display__item">
             b2
