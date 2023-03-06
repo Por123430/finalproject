f033da0783c6b64d824d00a8cce4a2b74cdbaca6
Por123430 <por123430@gmail.com>
2023-03-06 17:45:56 +0700

    แก้ดูuserให้แสดงได้แล้ว

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     baseUrl : 'http://localhost:3500',
     credentials: 'include',
-    prerareHeaders: (headers, { getState }) => {
+    prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
 
         if (token) {
@@ -15,10 +16,30 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWitchReauth = async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions)
+
+    if (result?.error?.status === 403) {
+        console.log('sending refresh token')
+
+        const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
+
+        if (refreshResult?.data) {
+
+            api.dispatch(setCredentials({ ...refreshResult.data}))
+
+            result = await baseQuery(args, api, extraOptions)
+        } else {
+            if (refreshResult?.error?.status === 403) {
+                refreshResult.error.data.message = "Your login has expired. "
+            }
+            return refreshResult
+        }
+    }
+
+    return result
 }
 export const apiSlice = createApi({
     // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
-    baseQuery,
-    tagTypes: ['Note', 'User', 'Moniters'],
+    baseQuery: baseQueryWitchReauth,
+    tagTypes: ['Note', 'Users', 'Moniters'],
     endpoints: builder => ({})
 })
